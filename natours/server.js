44015022e2d827b751d6002101db2117bcd6bd10
webdiego <webdiegomassarini@gmail.com>
2022-12-16 23:39:58 +0100

    feat(db):add prisma + postgres

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 // console.log(app.get('env')); //development set by express
 // console.log(process.env); // from node
 
