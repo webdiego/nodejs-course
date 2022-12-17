567f949abf2f2bf0359497988b0eb0410294de97
webdiego <webdiegomassarini@gmail.com>
2022-12-17 19:20:52 +0100

    feat(tour):add handle error

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,67 +1,83 @@
 const prisma = require('../prisma.client');
 
-const getAllTours = async (req, res) => {
-  const tours = await prisma.tour.findMany();
-  res
-    .status(200)
-    .json({ status: 'success', requested: req.requestTime, data: { tours } });
-};
+//TODO: add validator--> ZOD?
 
-const getTour = async (req, res) => {
-  const tour = await prisma.tour.findUnique({
-    where: {
-      id: Number(req.params.id),
-    },
-  });
-
-  if (!tour) {
+const getAllTours = async (req, res, next) => {
+  try {
+    const tours = await prisma.tour.findMany();
+    res
+      .status(200)
+      .json({ status: 'success', requested: req.requestTime, data: { tours } });
+  } catch (error) {
     res.status(400).json({ status: 'error', message: 'Tour not found' });
-    throw new Error('Tour not found');
+    next(error);
   }
-
-  res.status(200).json({ status: 'success', data: { tour } });
 };
 
-const addTour = async (req, res) => {
-  //TODO: add validator--> ZOD?
-  const { name, rating, price } = req.body;
-  const newTour = await prisma.tour.create({
-    data: {
-      name,
-      rating,
-      price,
-    },
-  });
-
-  // if (!newTour) {
-  //   res.status(400).json({ status: 'error', message: 'Tour not created' });
-  //   throw new Error('Tour not created');
-  // }
-  res.status(201).json({ status: 'success', data: { tour: newTour } }); //201 CREATED
+const getTour = async (req, res, next) => {
+  try {
+    const tour = await prisma.tour.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json({ status: 'success', data: { tour } });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: 'Tour not found' });
+    next(error);
+  }
 };
 
-const updateTour = async (req, res) => {
-  const { name, rating, price } = req.body;
-  const tour = await prisma.tour.update({
-    where: {
-      id: Number(req.params.id),
-    },
-    data: {
-      name,
-      rating,
-      price,
-    },
-  });
-  res.status(200).json({ status: 'success', data: { tour: tour } });
+const addTour = async (req, res, next) => {
+  try {
+    const { name, rating, price } = req.body;
+    const newTour = await prisma.tour.create({
+      data: {
+        name,
+        rating,
+        price,
+      },
+    });
+    res.status(200).json({ status: 'success', data: { tour: newTour } });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: 'Tour not created' });
+
+    next(error);
+  }
 };
 
-const deleteTour = async (req, res) => {
-  await prisma.tour.delete({
-    where: {
-      id: Number(req.params.id),
-    },
-  });
-  res.status(204).json({ status: 'success', data: null });
+const updateTour = async (req, res, next) => {
+  try {
+    const { name, rating, price } = req.body;
+    const tour = await prisma.tour.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name,
+        rating,
+        price,
+      },
+    });
+    res.status(200).json({ status: 'success', data: { tour: tour } });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: 'Tour not updated' });
+    next(error);
+  }
+};
+
+const deleteTour = async (req, res, next) => {
+  try {
+    await prisma.tour.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(204).json({ status: 'success', data: null });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: 'Tour not found' });
+    next(error);
+  }
 };
 
 module.exports = {
