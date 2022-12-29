42801cc51a6273df4c15327e59267b576a6cb0b5
webdiego <webdiegomassarini@gmail.com>
2022-12-29 21:30:15 +0100

    feat(app): rate-limiter

--- natours/app.js
+++ natours/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const notFound = require('./middleware/notFound');
 
+//Rate limiter
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1h
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 1h)
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
 const app = express();
+
 // Middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // GET /api/v1/tours 200 1.637 ms - 8628
 }
+app.use('/api', limiter);
 app.use(express.json());
 
 //Routes
