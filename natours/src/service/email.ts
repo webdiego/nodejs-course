5a19495684cd6fb948354b30667c9e67e3abdafd
webdiego <webdiegomassarini@gmail.com>
2023-01-03 02:05:09 +0100

    update(nodeMailer): add types and workaround

--- natours/src/service/email.ts
+++ natours/src/service/email.ts
@@ -2,9 +2,10 @@ import nodeMailer from 'nodemailer';
 
 export const sendEmail = async (options) => {
   // 1) Create a transporter
+
   const transporter = nodeMailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    host: String(process.env.EMAIL_HOST),
+    port: Number(process.env.EMAIL_PORT),
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
